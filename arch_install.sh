ad836031e95fbe97099534cd7b7bc946aca1a932
bursday <poutine@trump.localdomain>
2017-06-07 00:35:20 -0500

    make updates, more skrips part to need

--- arch_install.sh
+++ arch_install.sh
@@ -22,3 +22,24 @@ parted -s -a optimal /dev/sdb name 4 rootfs
 parted -s -a optimal /dev/sdb set 2 boot on
 
 
+#make file systems!
+mkfs.ext2 /dev/sdb2
+mkfs.ext4 /dev/sdb4
+#mount partitions
+mount /dev/sdb4 /mnt
+mkdir /mnt/boot
+mount /dev/sdb2 /mnt/boot
+
+#Edit Mirrors
+
+
+pacstrap /mnt base base-devel
+genfstab -U /mnt >> /mnt/etc/fstab
+#copy skrip and run the chroot one? hopefully?
+cp chroot.sh /mnt && arch-chroot /mnt ./chroot.sh
+
+
+
+
+
+
--- chroot.sh
+++ chroot.sh
@@ -6,11 +6,25 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo trump > /etc/hostname
 echo "#<ip-address>	<hostname.domain.org>	<hostname>" >> /etc/hosts
-echo "127.0.0.1	localhost.localdomain	localhost" /etc/hosts
-echo "::1		localhost.localdomain	localhost" /etc/hosts
-echo "127.0.0.1	trump.localdomain	trump" /etc/hosts
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "::1		localhost.localdomain	localhost" >> /etc/hosts
+echo "127.0.0.1	trump.localdomain	trump" >> /etc/hosts
 
 passwd
+
+#bootloader
+pacman -S --noconfirm grub
+grub-install /dev/sdb
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "###################\n"
+echo "##umount -R /mnt###\n"
+echo "######reboot#######\n"
+echo "###################\n"
+pause
+echo "press any key to continue"
+#todo: add a joke...not that one...this requires a script running AFTER chroot. good luck.
+#cant chroot again, because inception
 
 #I'm out...
 
