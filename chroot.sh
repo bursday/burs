50cff9122721e589587594ee4ea4f2f2dfe41421
bursday <bursday@gmx.com>
2017-06-09 03:07:27 -0500

    Create chroot.sh
    made some edit, for make better

--- chroot.sh
+++ chroot.sh
@@ -4,13 +4,12 @@ ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen
 echo trump > /etc/hostname
 echo "#<ip-address>	<hostname.domain.org>	<hostname>" >> /etc/hosts
 echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
-echo "::1		localhost.localdomain	localhost" >> /etc/hosts
+echo "::1 localhost.localdomain	localhost" >> /etc/hosts
 echo "127.0.0.1	trump.localdomain	trump" >> /etc/hosts
-
-passwd
 
 #bootloader
 pacman -S --noconfirm grub
