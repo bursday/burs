0ad0db5a6f2ce5dddb5f6c32e08a869948abab38
bursday <fjord@trump.localdomain>
2017-06-10 08:19:32 -0500

    make change to make skrip great again

--- chroot.sh
+++ chroot.sh
@@ -5,11 +5,13 @@ hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen
-echo trump > /etc/hostname
+echo "Please enter the name of the most honorable standard user san, kindly."
+read usersan
+echo $usersan > /etc/hostname
 echo "#<ip-address>	<hostname.domain.org>	<hostname>" >> /etc/hosts
 echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
 echo "::1 localhost.localdomain	localhost" >> /etc/hosts
-echo "127.0.0.1	trump.localdomain	trump" >> /etc/hosts
+echo "127.0.0.1	$usersan.localdomain	$usersan" >> /etc/hosts
 
 #bootloader
 pacman -S --noconfirm grub
